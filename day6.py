c7ac8ca9f741fc927ec73c04b7f368488e6412b0
Tim Schagen <schagen34@gmail.com>
2021-12-07 16:39:13 +0100

    Remove embarassing code from day 6

--- day6.py
+++ day6.py
@@ -3,24 +3,6 @@ with open('inputs/day6.txt') as f:
 
 fishes = list(map(int, lines[0].split(",")))
 
-def simulationStep(fish):
-    for ind in reversed(range(0, len(fish))):
-        if(fish[ind] == 0):
-            fish.append(8)
-            fish[ind] = 6
-            continue
-        fish[ind] -= 1
-    return fish
-
-def simulation(cycles):
-    fishies = fishes.copy()
-    for num in range(0, cycles):
-        fishies = simulationStep(fishies)
-    return len(fishies)
-
-
-print("part 1: " + str(simulation(80)))
-
 def fastSimulation(cycles):
     sim = [0] * 9
     for ind in fishes:
@@ -30,5 +12,5 @@ def fastSimulation(cycles):
         sim[(day + 7) % len(sim)] += sim[day]
     return sum(sim)
 
-
+print("part 1: " + str(fastSimulation(80)))
 print("part 2: " + str(fastSimulation(256)))
